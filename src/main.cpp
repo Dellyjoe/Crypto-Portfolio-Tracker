ed5a450cdbcac8445dacefd511a6c89ec11b1752
Daniel Morris <d.morris1225@gmail.com>
2021-04-22 19:52:45 -0400

    Dan's first commit...yay

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,6 @@ int main() {
     cout << "hello world" << endl;
     cout << "Joe's code base is up and running." << endl;
     cout << "Waiting for Dan's code base..." << endl;
+    cout << "First Commit Completed" << endl;
     return (0);
 }
